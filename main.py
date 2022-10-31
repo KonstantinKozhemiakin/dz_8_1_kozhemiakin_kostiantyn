30b43266b282b40362b2a780aa3d4c3d680a94dc
kozhemiakin_kostiantyn <kozhemiakin.konstantin@gmail.com>
2022-10-31 20:54:53 +0200

    third commit py_charm.py in test

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ def main_function():
 def test_function():
     print("Fifth commit, branch test")
 
+def main_function_last():
+    print('third commit py_charm.py in test')
+
+
 main_function()
 test_function()
