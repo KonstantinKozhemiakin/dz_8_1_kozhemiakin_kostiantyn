3ba8466a4706a29f4bbbf9c6d109edc1f273ae53
kozhemiakin_kostiantyn <kozhemiakin.konstantin@gmail.com>
2022-10-31 19:55:04 +0200

    fifth commit, branch test

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ def main_function():
     print("First commit")
     print("Second commit")
     print("Third commit")
+def test_function():
+    print("Fifth commit, branch test")
+
 main_function()
+test_function()
